526d4974d6dd0b1d46592aa7869c52d1b552b3ae
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2025-03-31 21:24:57 +0300

    Add assetsPrePath

--- src/app/[documentName]/_components/Document.tsx
+++ src/app/[documentName]/_components/Document.tsx
@@ -3,7 +3,6 @@
 import { Markdown } from '@ruslan-sedziukh/md-render'
 import { MarkdownProvider } from '../_context/MarkdownContext'
 import { ParsedMarkdown } from '@ruslan-sedziukh/md-types'
-import Image from './Image'
 
 type Props = {
   parsedMarkdown: ParsedMarkdown
@@ -13,7 +12,7 @@ type Props = {
 const Document = ({ parsedMarkdown, post }: Props) => {
   return (
     <MarkdownProvider post={post}>
-      <Markdown parsedMarkdown={parsedMarkdown} components={{ img: Image }} />
+      <Markdown parsedMarkdown={parsedMarkdown} />
     </MarkdownProvider>
   )
 }
---
+++ src/utils/consts.ts
@@ -0,0 +1,5 @@
+/**
+ * Path to folder with markdown documents.
+ * It should be a folder inside `public` directory.
+ *  */
+export const MD_DOCUMENTS_PATH = 'md-documents'
