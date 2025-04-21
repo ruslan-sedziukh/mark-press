2cfb871e6af0e4bbb3a966f97b664df8fd7b289c
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2025-04-21 15:03:03 +0300

    Add optional scroll to modal

--- src/app/[documentName]/_components/Sibebar/components/ModalContent/ModalContent.tsx
+++ src/app/[documentName]/_components/Sibebar/components/ModalContent/ModalContent.tsx
@@ -7,7 +7,9 @@ type Props = {
 const ModalContent = ({ children }: Props) => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-300/50 p-4 pb-20">
-      <div className="bg-neutral-50 h-[100%] p-4 rounded">{children}</div>
+      <div className="bg-neutral-50 h-[100%] p-4 rounded overflow-auto">
+        {children}
+      </div>
     </div>
   )
 }
